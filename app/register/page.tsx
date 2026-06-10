bd717cc47eb8b394aa269bfdb2983a72789bff96
huynqhe186603-prog <pda7558@gmail.com>
2026-06-10 19:50:42 +0700

    fix: remove unused Suspense import from register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Suspense, useState } from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { Loader2, Mail, Lock, User, AlertCircle } from 'lucide-react'
